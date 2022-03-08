7a6fee444c42191d99acb7be0eeb97a9da52cdaf
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-03-08 12:57:27 -0500

    more json edits

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import {AiOutlineHome} from 'react-icons/ai'
+import {AiFillHome} from 'react-icons/ai'
 
 function NavBar() {
   return (
     <nav className='navbar'>
-      <NavLink exact to="/"> <AiOutlineHome /> </NavLink>
+      <NavLink exact to="/"> <AiFillHome /> </NavLink>
       <NavLink exact to="/rankings">Rankings</NavLink>
       <NavLink exact to="/redemption">Redemption</NavLink>
       <NavLink exact to="/new-post">New Post</NavLink>
