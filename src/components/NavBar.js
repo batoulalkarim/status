0ba3e7c8822b489bbff42531a7b9a7e4c640df8c
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-03-08 12:24:03 -0500

    changed up some images

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import {AiOutlineHome} from 'react-icons/ai'
 
 function NavBar() {
   return (
     <nav className='navbar'>
-      <NavLink exact to="/">Home</NavLink>
+      <NavLink exact to="/"> <AiOutlineHome /> </NavLink>
       <NavLink exact to="/rankings">Rankings</NavLink>
-      <NavLink exact to="/retribution">Retribution</NavLink>
+      <NavLink exact to="/redemption">Redemption</NavLink>
       <NavLink exact to="/new-post">New Post</NavLink>
     </nav>
   )
